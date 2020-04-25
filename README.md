7804a3ca6c49e0ebce7ea057285e9c533a7259bd
mariopino <info@quequiereshacer.es>
2020-04-25 13:36:45 +0200

    Prompt user to enter password

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Kusama Session Keys
 
-Rotate session keys for your Kusama validator node with a single command!
+Rotate session keys for your Kusama validator node with a single command. This is intented to run locally on validator node.
 
 Based on a [script](https://paste.ubuntu.com/p/MhKM8vjVcD/) from [@fgimenez](https://github.com/fgimenez).
 
@@ -16,23 +16,22 @@ npm install
 
 From [Polkadot JS UI](https://polkadot.js.org/apps/#/accounts) export your account to json format and copy to keystores folder.
 
-**NOTE:** Remember to [clear bash history](https://askubuntu.com/questions/191999/how-to-clear-bash-history-completely) after execution to ensure your password is not stored anywhere.
-
 **Usage:**
 
 ```
-node index.js <account-export.json> <account-password>
+node index.js <account_json_export_file_path>
 ```
 
 **Example:**
 
 ```
-node index.js keystores/controller.json 1234567890
+node index.js keystores/controller.json
 ```
 
 Output:
 
 ```
+Please enter password:
 Importing account GTzRQPzkcuynHgkEHhsPBFpKdh4sAacVRsnd8vYfPpTMeEY
 Connected to node websocket ws://localhost:9944
 New generated keys are 0xaaa287115f15d66cdae2dcf0e0416bd89a221c4dc62b6cc40bdbbc224e72f2fc5a382991d73337d051b95cd61a193fe2a4ab47087ed15d1d9e47fdd1eb8e390e94a682cbe5b5194bbba9d06a33e9c89d13ec05b0ac26bdb446d4521581bf1a2b4cfc4f58da7336c6728bcbc57f64d4189d02bc624de88c85f29331b1f7fad01a7692b9d4a4655fd4f49e624536f272bd1f564e2957eebfe8ae170b26c7507371
--- index.js
+++ index.js
@@ -5,8 +5,9 @@ keyring.initKeyring({
 });
 const fs = require('fs');
 
-if (process.argv.length !== 4) {
-  console.error('Usage: node index.js keystores/controller.json 1234567890');
+if (process.argv.length !== 3) {
+  console.error('Usage: node index.js <account_json_export_file_path>');
+  console.error('Example: node index.js keystores/controller.json');
   process.exit(1);
 }
 
@@ -20,41 +21,48 @@ const password = (process.argv[3] || ``);
 const wsProvider = `ws://localhost:9944`;
 
 const main = async () => {
-
-  // Read and import account using provided password
-  let raw = fs.readFileSync(accountJSON, { encoding: 'utf-8' });
-  const account = JSON.parse(raw);
-  const address = account.address;
-  console.log(`Importing account`, address)
-  const signer = keyring.restoreAccount(account, password); 
-  signer.decodePkcs8(password);
-
-  // Connect to node
-  const provider = new WsProvider(wsProvider);
-  const api = await ApiPromise.create({ provider });
-  console.log(`Connected to node websocket`, wsProvider);
-
-  // Get new session keys
-  const newKeys = await api.rpc.author.rotateKeys();
-  console.log(`New generated keys are`, newKeys.toHex());
-
-  // Get account nonce
-  const nonce = (await api.derive.balances.account(address)).accountNonce
-  console.log(`Account nonce is`, nonce.toHuman());
-
-  // Send set keys tx
-  const hash = await api.tx.session.setKeys(
-    newKeys,
-    [0],
-  ).signAndSend(signer, { nonce });
-  console.log(`Success transaction! tx hash is`, hash.toString());
-
-  process.exit(1);
   
+  // Prompt user to enter password
+  const standard_input = process.stdin;
+  standard_input.setEncoding('utf-8');
+  console.log("Please enter password:");
+  standard_input.on('data', function (data) {
+
+    // Read and import account using provided password
+    let raw = fs.readFileSync(accountJSON, { encoding: 'utf-8' });
+    const account = JSON.parse(raw);
+    const address = account.address;
+    console.log(`Importing account`, address)
+    const signer = keyring.restoreAccount(account, password); 
+    signer.decodePkcs8(password);
+
+    // Connect to node
+    const provider = new WsProvider(wsProvider);
+    const api = await ApiPromise.create({ provider });
+    console.log(`Connected to node websocket`, wsProvider);
+
+    // Get new session keys
+    const newKeys = await api.rpc.author.rotateKeys();
+    console.log(`New generated keys are`, newKeys.toHex());
+
+    // Get account nonce
+    const nonce = (await api.derive.balances.account(address)).accountNonce
+    console.log(`Account nonce is`, nonce.toHuman());
+
+    // Send set keys tx
+    const hash = await api.tx.session.setKeys(
+      newKeys,
+      [0],
+    ).signAndSend(signer, { nonce });
+    console.log(`Success transaction! tx hash is`, hash.toString());
+
+    process.exit(1);
+  });
+  process.exit();
 }
 
 try {
   main();
-} catch (ERR) {
-  console.error(ERR);
+} catch (error) {
+  console.error(error);
 }
