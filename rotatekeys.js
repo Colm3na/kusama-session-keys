0f737bb5a2714cbbb6a9f70b58d74059baa988df
mariopino <info@quequiereshacer.es>
2020-04-25 18:48:30 +0200

    better param parsing, add password and log params

--- rotatekeys.js
+++ rotatekeys.js
@@ -5,18 +5,41 @@ keyring.initKeyring({
 });
 const fs = require('fs');
 const prompts = require('prompts');
+const yargs = require('yargs');
 
-if (process.argv.length !== 3) {
-  console.error('Usage: node index.js <account_json_export_file_path>');
-  console.error('Example: node index.js keystores/controller.json');
-  process.exit(1);
-}
+const argv = yargs
+  .scriptName("rotatekeys.js")
+  .option('controller', {
+    alias: 'c',
+    description: 'Controller account json file path',
+    type: 'string',
+  })
+  .option('password', {
+      alias: 'p',
+      description: 'Controller account password, if not set stdin will be used',
+      type: 'string',
+  })
+  .option('log', {
+    alias: 'l',
+    description: 'log session keys to file',
+    type: 'boolean',
+  })
+  .demandOption(['controller'], 'Please provide the controller account json file path')
+  .usage("node rotatekeys.js -c controller.json -p password")
+  .help()
+  .alias('help', 'h')
+  .alias('version', 'v')
+  .version()
+  .argv;
 
-// Account file param
-const accountJSON = (process.argv[2] || `keystores/controller.json`);
+// Controller account json file
+const controller = argv.controller;
 
 // Password param
-const password = (process.argv[3] || ``);
+const password = argv.password || false;
+
+// Logging to file param
+const log = argv.log || false;
 
 // Node websocket
 const wsProvider = `ws://localhost:9944`;
@@ -27,19 +50,22 @@ const main = async () => {
   console.log("\x1b[1m - Check source at https://github.com/Colm3na/kusama-session-keys\x1b[0m");
   console.log("\x1b[32m\x1b[1m - Made with love from ColmenaLabs_SVQ https://colmenalabs.org/\x1b[0m\n");
 
-  let raw = fs.readFileSync(accountJSON, { encoding: 'utf-8' });
+  let raw = fs.readFileSync(controller, { encoding: 'utf-8' });
   const account = JSON.parse(raw);
   const address = account.address;
   
   // Prompt user to enter password
-  const { password } = await prompts({
-    type: 'password',
-    name: 'password',
-    message: `Enter password for ${address}:`
-  });
+  if (!password) {
+    const response = await prompts({
+      type: 'password',
+      name: 'password',
+      message: `Enter password for ${address}:`
+    });
+    password = response.password;
+  }
 
   if (password) {
-    
+
     console.log(`\n\x1b[1m -> Importing account\x1b[0m`, address)
     const signer = keyring.restoreAccount(account, password); 
     signer.decodePkcs8(password);
@@ -62,6 +88,10 @@ const main = async () => {
       [0],
     ).signAndSend(signer, { nonce });
     console.log(`\n\x1b[32m\x1b[1mSuccess! \x1b[37mCheck tx in PolkaScan: https://polkascan.io/pre/kusama/transaction/${hash.toString()}\x1b[0m\n`);
+
+    if (log) {
+      fs.appendFileSync(`sessionkeys.log`, `${new Date()} - ${hash.toString()}`);
+    }
 
     process.exit(1);
   }
