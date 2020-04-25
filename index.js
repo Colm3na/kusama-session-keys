6713ad8eb757463a87df40c39c169b9fddd5b8ab
mariopino <info@quequiereshacer.es>
2020-04-25 14:47:23 +0200

    final touchs

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const wsProvider = `ws://localhost:9944`;
 const main = async () => {
 
   console.log("\n\x1b[45m\x1b[1m Rotate Kusama validator session keys \x1b[0m\n");
-  console.log("\x1b[1m -> Check source at https://github.com/Colm3na/kusama-session-keys\x1b[0m");
-  console.log("\x1b[32m\x1b[1m -> Made with love in La Colmena Labs https://colmenalabs.org/\x1b[0m\n");
+  console.log("\x1b[1m - Check source at https://github.com/Colm3na/kusama-session-keys\x1b[0m");
+  console.log("\x1b[32m\x1b[1m - Made with love in La Colmena Labs https://colmenalabs.org/\x1b[0m\n");
 
   let raw = fs.readFileSync(accountJSON, { encoding: 'utf-8' });
   const account = JSON.parse(raw);
@@ -61,7 +61,7 @@ const main = async () => {
       newKeys,
       [0],
     ).signAndSend(signer, { nonce });
-    console.log(`\n\x1b[31m\x1b[1mSuccess! \x1b[37mCheck tx in PolkaScan: https://polkascan.io/pre/kusama/transaction/${hash.toString()}\x1b[0m\n`);
+    console.log(`\n\x1b[32m\x1b[1mSuccess! \x1b[37mCheck tx in PolkaScan: https://polkascan.io/pre/kusama/transaction/${hash.toString()}\x1b[0m\n`);
 
     process.exit(1);
   }
